6050ff9b9e4b0f8a47d9aa114c7067e8734e2963
sushil5 <37663466+sushil5@users.noreply.github.com>
2018-08-14 18:59:31 +0530

    created Calculator
    simple Calculator with 2 add and sub methods(it was just a trial)

---
+++ calculator.java
@@ -0,0 +1,20 @@
+class Calculator{
+
+public static void main(String args[]){
+
+Calculator c=new Calculator();
+c.add(10,5);
+c.sub(20,9);
+
+
+}
+public void add(int a,int b){
+System.out.println("The addition is:"+(a+b));
+
+}
+public void sub(int a,int b){
+System.out.println("The subtraction is:"+(a-b));
+
+}
+
+}
